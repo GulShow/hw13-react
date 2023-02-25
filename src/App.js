d6edb29539e8b9987b198a4960d95f8a8070b2d1
Stanislav Huliaiev <101598952+GulShow@users.noreply.github.com>
2023-02-26 00:41:17 +0200

    final commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import MyCard from './components/MyCard';
+
+function App() {
+  return (
+    <div className="App">
+     <MyCard></MyCard>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/MyCard.jsx
@@ -0,0 +1,12 @@
+import { Card } from 'react-bootstrap';
+
+export default function MyCard() {
+  return (
+    <Card>
+      <Card.Body>
+        <Card.Title>Title</Card.Title>
+        <Card.Text>Text</Card.Text>
+      </Card.Body>
+    </Card>
+  );
+}
